46cc59d6e096f47790a1fc4f4f717fc9dfa36c0c
Liam Stabeno <liamstabeno@gmail.com>
2017-02-21 09:08:11 -0800

    impliments the for loop instead of populating the array with multiple push methods

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,14 @@ $("document").ready(function() {
 
     var favArray = [color, band, food]; //creates our array
     var sndArray = []; // makes an empty array
-    sndArray.push(favArray[1]); // add item #1 in our original array into our empty array
-    sndArray.push(favArray[0]);
-    sndArray.push(favArray[2]);
+
+    for(i=0; i<favArray.length; i ++){ // uses the for loop to populate the sndArray
+      sndArray.push(favArray[i])
+    };
+
+    // sndArray.push(favArray[1]); // add item #1 in our original array into our empty array
+    // sndArray.push(favArray[0]);
+    // sndArray.push(favArray[2]);
     alert(sndArray); // displays the new order of our array
     $("#result").append("<li>"+sndArray[0]+"</li>"); // put first array item in webpage as the first bullet in a list
     $("ul").append("<li>"+sndArray[1]+"</li>");
